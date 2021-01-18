ca3aaf3f88acbc44f6fe9de0d0845cab2a9533f9
Manoj <thorimanoj@gmail.com>
2021-01-18 15:48:21 +0530

    title updated

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Code Example',
+      title: 'Flutter Code Sample',
       home: BottomNavBarExample(),
     );
   }
